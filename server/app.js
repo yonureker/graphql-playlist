c1d81e5309f6174baba21fe6102c49d9ca50c970
yonureker <yonureker@gmail.com>
2020-01-27 12:32:18 -0800

    first commit

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000');
+})
