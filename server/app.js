2e9eb1c82ea847d0cbe16d4fbaff73153c8ebb04
yonureker <yonureker@gmail.com>
2020-01-28 14:44:46 -0800

    add cors and mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose')
+const db = require('./config/keys')
+const cors = require('cors');
 
 const app = express();
+
+//allow cross origin requests
+app.use(cors());
+
+//connect to mongo database
+mongoose.connect(db.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => console.log('connected to database'));
 
 app.use('/graphql', graphqlHTTP({
   schema,
